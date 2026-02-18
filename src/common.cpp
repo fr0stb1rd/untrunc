3e1fa43c06d3cca84ab2cdc6af325f135f074b17
fr0stb1rd <fr0stb1rd@proton.me>
2026-02-18 19:23:56 +0300

    refactor: Remove `_RPCNDR_H_NO_BYTE_TYPEDEF` CXXFLAG and add `WIN32_LEAN_AND_MEAN` to `common.cpp` for Windows builds.

--- src/common.cpp
+++ src/common.cpp
@@ -479,6 +479,7 @@ void parseMaxPartsize(string& s) {
 }
 
 #ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN  // Exclude COM/OLE headers that conflict with std::byte (GCC 15+)
 #include <windows.h>
 #include <codecvt>
 
